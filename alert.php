412b51be0c09d95a18c0ca9bfa8fc1b4da226b11
Russell250 <89474081+Russell250@users.noreply.github.com>
2022-12-05 20:09:28 -0600

    Create alert.php

---
+++ alert.php
@@ -0,0 +1,17 @@
+<?php require_once("header.php"); ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Alert</h2>
+
+<button onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  alert("Thanks for clicking me!");
+}
+</script>
+
+</body>
+</html>
